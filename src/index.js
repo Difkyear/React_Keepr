27b77ba8885838907344306d8e18cae38eeaf972
Vincent Yeh <difkye247@gmail.com>
2024-05-13 15:07:05 +0800

    add React Router

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-ReactDOM.render(<App />, document.getElementById("root"));
+import Login from "./components/page/Login";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+let router = createBrowserRouter([
+  {
+    path: "/Login",
+    element: <Login />,
+    errorElement: <h1>404 Not Found</h1>,
+  },
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.render(
+  <RouterProvider router={router} />,
+  document.getElementById("root")
+);
